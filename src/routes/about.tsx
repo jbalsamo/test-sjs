ed6f78a1c5b639247920556870360a5aee5557d2
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2022-12-25 01:12:44 -0500

    feat: :art: Initial Commit

---
+++ src/routes/about.tsx
@@ -0,0 +1,12 @@
+import { Title } from "solid-start";
+import Counter from "~/components/Counter";
+
+export default function About() {
+  return (
+    <main>
+      <Title>About Logos</Title>
+      <h1>About Logos Affiliates</h1>
+      <p>We are the premiere source for all things in programming.</p>
+    </main>
+  );
+}
